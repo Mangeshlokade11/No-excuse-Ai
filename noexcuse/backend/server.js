c97106bb3786636cbcfc1a1b156d20a25f55704c
Mangesh347 <31231102@vupune.ac.in>
2026-03-29 18:08:05 +0530

    fix cors + api

--- noexcuse/backend/server.js
+++ noexcuse/backend/server.js
@@ -7,7 +7,7 @@ app.use(cors({
 require('dotenv').config();
 const express  = require('express');
 const mongoose = require('mongoose');
-const cors     = require('cors');
+
 const path     = require('path');
 
 const app = express();
